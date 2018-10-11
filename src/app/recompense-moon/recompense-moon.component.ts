33b4db438e21f77380b219ad96a84ad037ad023a
PsklRenaud <wildcodeschool@pascal-renaud.com>
2018-10-11 21:47:57 +0200

    thumb fini

--- src/app/recompense-moon/recompense-moon.component.ts
+++ src/app/recompense-moon/recompense-moon.component.ts
@@ -10,16 +10,40 @@ export class RecompenseMoonComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    function changeImage(element)
-{
-  var x = element.document.getElementById("bigImg");
-  var v = x.getAttribute("");
-  if(v == "feed-blue.png")
-    v = "feed-orange.png");
-  else
-    v = "feed-blue.png");
-  x.setAttribute("src", v);	
-}
+    const bigImg= document.getElementById("bigImg");
+    const thumb1= document.getElementById("thumb1");
+    const thumb2= document.getElementById("thumb2");
+    const thumb3= document.getElementById("thumb3");
+    const thumb4= document.getElementById("thumb4");
+    const thumb2b= document.getElementById("thumb2b");
+    const thumb3b= document.getElementById("thumb3b");
+    const thumb4b= document.getElementById("thumb4b");
+    
+    thumb1.addEventListener("click", function() {
+      bigImg.classList.remove("hidden");
+      thumb2b.classList.add("hidden")
+      thumb3b.classList.add("hidden")
+      thumb4b.classList.add("hidden");
+    });
+    thumb2.addEventListener("click", function() {
+      bigImg.classList.add("hidden");
+      thumb2b.classList.remove("hidden")
+      thumb3b.classList.add("hidden")
+      thumb4b.classList.add("hidden");
+    });
+    thumb3.addEventListener("click", function() {
+      bigImg.classList.add("hidden");
+      thumb2b.classList.add("hidden")
+      thumb3b.classList.remove("hidden")
+      thumb4b.classList.add("hidden");
+    });
+    thumb4.addEventListener("click", function() {
+      bigImg.classList.add("hidden");
+      thumb2b.classList.add("hidden")
+      thumb3b.classList.add("hidden")
+      thumb4b.classList.remove("hidden");
+    });
+   
   }
 
 }
