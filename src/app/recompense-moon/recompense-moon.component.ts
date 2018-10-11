76129a74d6f79d891c7d9a61aeed62a736e8bf16
PsklRenaud <wildcodeschool@pascal-renaud.com>
2018-10-11 21:03:48 +0200

    a supp

--- src/app/recompense-moon/recompense-moon.component.ts
+++ src/app/recompense-moon/recompense-moon.component.ts
@@ -10,6 +10,16 @@ export class RecompenseMoonComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    function changeImage(element)
+{
+  var x = element.document.getElementById("bigImg");
+  var v = x.getAttribute("");
+  if(v == "feed-blue.png")
+    v = "feed-orange.png");
+  else
+    v = "feed-blue.png");
+  x.setAttribute("src", v);	
+}
   }
 
 }
