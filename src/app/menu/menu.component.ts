cde2a4222ed020e635b6c2cb459422084fd728a8
PsklRenaud <wildcodeschool@pascal-renaud.com>
2018-10-23 17:51:58 +0200

    nav-bar fini

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -12,47 +12,53 @@ export class MenuComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-  //   const burger= document.getElementById("burger");
-  //   const burger2= document.getElementById("burger2");
-  //   const connexion= document.getElementById("connexion");
-  //   const sattelite= document.getElementById("sattelite");
-  //   const tableau= document.getElementById("tableau");
-  //   const sattelite2= document.getElementById("sattelite2");
-  //   const bigMenu=document.getElementById("bigMenu");
-  // // je remplace le bouton burger par le bouton burger2 et j'affiche les icones et liens 
-  //     burger.addEventListener("click", function() {
-  //     burger.classList.add("hidden");
-  //     burger2.classList.remove("hidden");
-  //     connexion.classList.remove("hidden");
-  //     sattelite.classList.remove("hidden");
-  //     tableau.classList.remove("hidden");
-  //     sattelite2.classList.remove("hidden");
-  //     bigMenu.classList.add("bigMenu");
-  //   });
-  // // je remplace le bouton burger2 par le bouton burger et je masque les icones et liens
-  //   burger2.addEventListener("click", function() {
-  //     burger2.classList.add("hidden");
-  //     burger.classList.remove("hidden");
-  //     connexion.classList.add("hidden");
-  //     sattelite.classList.add("hidden");
-  //     tableau.classList.add("hidden");
-  //     sattelite2.classList.add("hidden");
-  //     bigMenu.classList.remove("bigMenu");
-  //   });
+    //   const burger= document.getElementById("burger");
+    //   const burger2= document.getElementById("burger2");
+    //   const connexion= document.getElementById("connexion");
+    //   const sattelite= document.getElementById("sattelite");
+    //   const tableau= document.getElementById("tableau");
+    //   const sattelite2= document.getElementById("sattelite2");
+    //   const bigMenu=document.getElementById("bigMenu");
+    // // je remplace le bouton burger par le bouton burger2 et j'affiche les icones et liens 
+    //     burger.addEventListener("click", function() {
+    //     burger.classList.add("hidden");
+    //     burger2.classList.remove("hidden");
+    //     connexion.classList.remove("hidden");
+    //     sattelite.classList.remove("hidden");
+    //     tableau.classList.remove("hidden");
+    //     sattelite2.classList.remove("hidden");
+    //     bigMenu.classList.add("bigMenu");
+    //   });
+    // // je remplace le bouton burger2 par le bouton burger et je masque les icones et liens
+    //   burger2.addEventListener("click", function() {
+    //     burger2.classList.add("hidden");
+    //     burger.classList.remove("hidden");
+    //     connexion.classList.add("hidden");
+    //     sattelite.classList.add("hidden");
+    //     tableau.classList.add("hidden");
+    //     sattelite2.classList.add("hidden");
+    //     bigMenu.classList.remove("bigMenu");
+    //   });
   }
 
-showMenu() {
-  const bigMenu=document.getElementById("bigMenu");
-  const logo=document.getElementById("logo");
-  if (bigMenu.classList.contains("hidden")){
-    bigMenu.classList.remove("hidden");
-    bigMenu.classList.add("Onit");
-    logo.classList.add("logo")
-  } else {
+  showMenu() {
+    const bigMenu = document.getElementById("bigMenu");
+    const logo = document.getElementById("logo");
+    if (bigMenu.classList.contains("hidden")) {
+      bigMenu.classList.remove("hidden");
+      bigMenu.classList.add("Onit");
+      logo.classList.add("logo")
+    } else {
+      bigMenu.classList.add("hidden");
+      bigMenu.classList.remove("Onit");
+      logo.classList.remove("logo")
+    }
+  }
+  hideMenu() {
+    const bigMenu = document.getElementById("bigMenu");
+    const logo = document.getElementById("logo");
     bigMenu.classList.add("hidden");
     bigMenu.classList.remove("Onit");
     logo.classList.remove("logo")
   }
-}
-
 }
